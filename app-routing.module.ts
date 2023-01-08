c67b912f2cd9d87547b38611d0574b3eda24c90c
maximeschwob <110344369+maximeschwob@users.noreply.github.com>
2023-01-08 21:30:47 +0100

    Add files via upload

---
+++ app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { LoginComponent } from './components/login/login.component';
+import { PnfComponent } from './components/pnf/pnf.component';
+import { RegisterComponent } from './components/register/register.component';
+ import { AuthGuard } from './guards/auth.guard';
+
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'login' },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  {
+    path: 'customers',
+    component: CustomersComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'contacts', component: ContactsComponent, canActivate: [AuthGuard] },
+  { path: '**', component: PnfComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ app.module.ts
@@ -0,0 +1,55 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { LoginComponent } from './components/login/login.component';
+// import { FooterComponent } from './components/footer/footer.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { CustomersComponent } from './components/customers/customers.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { PnfComponent } from './components/pnf/pnf.component';
+import { FormsModule } from '@angular/forms';
+import { FilterPipe } from './pipes/filter.pipe';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { EditCustomerComponent } from './components/edit-customer/edit-customer.component';
+import { FilterContactPipe } from './pipes/filter-contact.pipe';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { RegisterComponent } from './components/register/register.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    FooterComponent,
+    NavbarComponent,
+    CustomersComponent,
+    ContactsComponent,
+    PnfComponent,
+    FilterPipe,
+    CustomerDetailsComponent,
+    AddCustomerComponent,
+    EditCustomerComponent,
+    FilterContactPipe,
+    SideNavComponent,
+    RegisterComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    NgbModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
