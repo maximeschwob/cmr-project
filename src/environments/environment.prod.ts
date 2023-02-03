470a28617b225a95220ab2cbade7f531f3aacfd1
maxime schwob <maximeschwob26@icloud.com>
2023-02-03 11:14:25 +0100

    fixe for aviran

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'cmr-project-f64f3',
+    appId: '1:273507419751:web:67b35e204196943a4536c2',
+    storageBucket: 'cmr-project-f64f3.appspot.com',
+    apiKey: 'AIzaSyBxzOa9PhSCXxv0k7dmnuPZHk-GI08CGBU',
+    authDomain: 'cmr-project-f64f3.firebaseapp.com',
+    messagingSenderId: '273507419751',
+    measurementId: 'G-6BY70Z25VF',
+  },
   production: true
 };
